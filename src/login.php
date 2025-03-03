02dc95215012e0b0a491ec51d70532c1f6916aff
gabrynapo <gabri.napo03@gmail.com>
2025-03-03 17:25:05 +0100

    Form Login

---
+++ src/login.php
@@ -0,0 +1,14 @@
+<?php
+    require_once __DIR__ . '/config/db.php';
+?>
+
+<form action="login.php" method="POST">
+    <h1>Accedi</h1>
+    <h3>Email </h3>
+    <input type="text" id="email" placeholder="Email" name="email" maxlength="30" required>
+    <h3>Password </h3>
+    <input type="password" id="password" placeholder="Password" name="password" required>
+    <button type="submit" name="login">Accedi</button>
+</form>
+
+<p> Sei nuovo su Bostarter? <a href="register.php">Registrati</a><p>
